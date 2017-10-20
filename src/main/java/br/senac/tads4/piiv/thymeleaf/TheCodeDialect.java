f76765f7582362a40aaea10e6fb54562a83d9675
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-10-20 13:11:36 -0200

    Criado a classe MessageSiteElementTagProcessor para separar os templates de mensagens do backoffice e site. Feito o registro em TheCodeDialect.

--- src/main/java/br/senac/tads4/piiv/thymeleaf/TheCodeDialect.java
+++ src/main/java/br/senac/tads4/piiv/thymeleaf/TheCodeDialect.java
@@ -9,7 +9,8 @@ import org.thymeleaf.standard.StandardDialect;
 
 import br.senac.tads4.piiv.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
 import br.senac.tads4.piiv.thymeleaf.processor.FormProdutoElementTagProcessor;
-import br.senac.tads4.piiv.thymeleaf.processor.MessageElementTagProcessor;
+import br.senac.tads4.piiv.thymeleaf.processor.MessageBackofficeElementTagProcessor;
+import br.senac.tads4.piiv.thymeleaf.processor.MessageSiteElementTagProcessor;
 
 public class TheCodeDialect extends AbstractProcessorDialect {
 
@@ -21,8 +22,9 @@ public class TheCodeDialect extends AbstractProcessorDialect {
 	public Set<IProcessor> getProcessors(String dialectPrefix) {
 		final Set<IProcessor> processadores = new HashSet<>();
 		processadores.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
-		processadores.add(new MessageElementTagProcessor(dialectPrefix));
 		processadores.add(new FormProdutoElementTagProcessor(dialectPrefix));
+		processadores.add(new MessageBackofficeElementTagProcessor(dialectPrefix));
+		processadores.add(new MessageSiteElementTagProcessor(dialectPrefix));
 		return processadores;
 	}
 }
--- src/main/java/br/senac/tads4/piiv/thymeleaf/processor/MessageSiteElementTagProcessor.java
+++ src/main/java/br/senac/tads4/piiv/thymeleaf/processor/MessageSiteElementTagProcessor.java
@@ -8,12 +8,12 @@ import org.thymeleaf.processor.element.AbstractElementTagProcessor;
 import org.thymeleaf.processor.element.IElementTagStructureHandler;
 import org.thymeleaf.templatemode.TemplateMode;
 
-public class MessageBackofficeElementTagProcessor extends AbstractElementTagProcessor {
+public class MessageSiteElementTagProcessor extends AbstractElementTagProcessor {
 
-	private static final String NOME_TAG = "messagebackoffice";
+	private static final String NOME_TAG = "messagesite";
 	private static final int PRECEDENCIA = 1000;
 	
-	public MessageBackofficeElementTagProcessor(String dialectPrefix) {
+	public MessageSiteElementTagProcessor(String dialectPrefix) {
 		super(TemplateMode.HTML, dialectPrefix, NOME_TAG, true, null, true, PRECEDENCIA);
 	}
 
@@ -23,8 +23,7 @@ public class MessageBackofficeElementTagProcessor extends AbstractElementTagProc
 		IModelFactory modelFactory = context.getModelFactory();
 		
 		IModel model = modelFactory.createModel();
-		model.add(modelFactory.createStandaloneElementTag("th:block", "th:replace", "layout/backoffice/fragments/MensagemSucesso"));
-		model.add(modelFactory.createStandaloneElementTag("th:block", "th:replace", "layout/backoffice/fragments/MensagensErroValidacao"));
+		model.add(modelFactory.createStandaloneElementTag("th:block", "th:replace", "layout/site/fragments/MensagensErroValidacao"));
 		
 		structureHandler.replaceWith(model, true);
 	}
