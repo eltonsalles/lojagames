b187d612a9bc7716345cfdc0cce4d0d0bd0757bd
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-10-20 13:12:22 -0200

    Arrumado os layouts para receberem o template das mensagens de alerta.

--- src/main/resources/static/layout/site/javascripts/thecode-site.js
+++ src/main/resources/static/layout/site/javascripts/thecode-site.js
@@ -1,73 +1,84 @@
 var interval = null;
 var lastScroll = 0;
 
-$(document).ready(function(){
-  // Navbar
-  $(".button-collapse").sideNav();
+$(document).ready(function() {
+	// Navbar
+	$(".button-collapse").sideNav();
 
-  // Remove o active no menu
-  $('.remove-indicator .tab a').removeClass('active');
+	// Remove o active no menu
+	$('.remove-indicator .tab a').removeClass('active');
 
-  // Carousel
-  $('.carousel.carousel-slider').carousel({fullWidth: true});
-  playInterval();
+	// Carousel
+	$('.carousel.carousel-slider').carousel({
+		fullWidth : true
+	});
+	playInterval();
 
-  // Adiciona funções aos eventos do carousel
-  $('.carousel.carousel-slider').on({
-    mouseenter: function() {
-      stopInterval();
-    },
-    mouseleave: function() {
-      playInterval();
-    }
-  });
+	// Adiciona funções aos eventos do carousel
+	$('.carousel.carousel-slider').on({
+		mouseenter : function() {
+			stopInterval();
+		},
+		mouseleave : function() {
+			playInterval();
+		}
+	});
 
-  // Função para iniciar o carousel
-  function playInterval() {
-    interval = setInterval(function() {
-      $('.carousel.carousel-slider').carousel('next');
-    }, 4500)
-  }
+	// Função para iniciar o carousel
+	function playInterval() {
+		interval = setInterval(function() {
+			$('.carousel.carousel-slider').carousel('next');
+		}, 4500)
+	}
 
-  // Função para parar o carousel
-  function stopInterval() {
-    clearInterval(interval);
-  }
+	// Função para parar o carousel
+	function stopInterval() {
+		clearInterval(interval);
+	}
 
-  // Inicia o parallax nas páginas
-  $('.parallax').parallax();
+	// Inicia o parallax nas páginas
+	$('.parallax').parallax();
 
-  // Inicia os campos selects com a formatação do materialize
-  $('select').material_select();
+	// Inicia os campos selects com a formatação do materialize
+	$('select').material_select();
 
-  // Esconde o botão voltar ao topo
-  $('.btn-voltar-topo').hide();
+	// Esconde o botão voltar ao topo
+	$('.btn-voltar-topo').hide();
 
-  // Verifica o scroll da página para mostrar o botão voltar ao topo
-  $(window).scroll(function() {
-    var pos = $(this).scrollTop();
+	// Verifica o scroll da página para mostrar o botão voltar ao topo
+	$(window).scroll(function() {
+		var pos = $(this).scrollTop();
 
-    if (pos > lastScroll) {
-      $('.menu-desktop').slideUp(400, function() {
-        $('.ocultar-navbar').slideUp(200);
-      });
-    } else {
-      $('.ocultar-navbar').slideDown(400, function() {
-        $('.menu-desktop').slideDown(200);
-      });
-    }
+		if (pos > lastScroll) {
+			$('.menu-desktop').slideUp(400, function() {
+				$('.ocultar-navbar').slideUp(200);
+			});
+		} else {
+			$('.ocultar-navbar').slideDown(400, function() {
+				$('.menu-desktop').slideDown(200);
+			});
+		}
 
-    lastScroll = pos;
+		lastScroll = pos;
 
-    if ($(this).scrollTop() > 300) {
-      $('.btn-voltar-topo').fadeIn();
-    } else {
-      $('.btn-voltar-topo').fadeOut();
-    }
-  });
+		if ($(this).scrollTop() > 300) {
+			$('.btn-voltar-topo').fadeIn();
+		} else {
+			$('.btn-voltar-topo').fadeOut();
+		}
+	});
 
-  // Atribui ao botão a função de voltar ao topo
-  $('.btn-voltar-topo').click(function() {
-    $('html, boby').animate({scrollTop: 0}, 2000);
-  });
+	// Atribui ao botão a função de voltar ao topo
+	$('.btn-voltar-topo').click(function() {
+		$('html, boby').animate({
+			scrollTop : 0
+		}, 2000);
+	});
+
+	// Código para fechar os alertas do materialize
+	$('body').on('click', '.tc-alert .tc-close', function() {
+		$(this).parent().fadeOut(300, function() {
+			$(this).remove();
+		});
+	});
 });
