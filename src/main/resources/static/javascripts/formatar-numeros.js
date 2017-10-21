097746c31953c03d16aa2271f94749288e2140cd
Fillipe Pinheiro <fillipe.pinheiro@outlook.com>
2017-10-21 11:32:54 -0200

    Validação do front de cadastro-controle e ajuste da formatação numérica.

--- src/main/resources/static/javascripts/formatar-numeros.js
+++ src/main/resources/static/javascripts/formatar-numeros.js
@@ -1,7 +1,7 @@
 $(function() {
 	var decimal = $('.js-decimal');
-	decimal.maskMoney({decimal: ',', thousands: '.'});
+	decimal.maskMoney({decimal: ',', thousands: '.', allowEmpty: true});
 	
 	var int = $('.js-int');
-	int.maskMoney({precision: 0, thousands: '.'});
+	int.maskMoney({precision: 0, thousands: '.', allowEmpty: true});
 });
