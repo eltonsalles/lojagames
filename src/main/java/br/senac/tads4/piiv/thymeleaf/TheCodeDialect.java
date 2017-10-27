7c797fcec890baeb5d361cf30aa57bd45db6a25a
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-10-27 11:35:10 -0200

    Criado a tag thecode:formimagemproduto para cadastrar imagem em todos os tipos de produto.

--- src/main/java/br/senac/tads4/piiv/thymeleaf/TheCodeDialect.java
+++ src/main/java/br/senac/tads4/piiv/thymeleaf/TheCodeDialect.java
@@ -8,6 +8,7 @@ import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
 
 import br.senac.tads4.piiv.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
+import br.senac.tads4.piiv.thymeleaf.processor.FormImagemProdutoElementTagProcessor;
 import br.senac.tads4.piiv.thymeleaf.processor.FormProdutoElementTagProcessor;
 import br.senac.tads4.piiv.thymeleaf.processor.MessageBackofficeElementTagProcessor;
 import br.senac.tads4.piiv.thymeleaf.processor.MessageSiteElementTagProcessor;
@@ -23,6 +24,7 @@ public class TheCodeDialect extends AbstractProcessorDialect {
 		final Set<IProcessor> processadores = new HashSet<>();
 		processadores.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
 		processadores.add(new FormProdutoElementTagProcessor(dialectPrefix));
+		processadores.add(new FormImagemProdutoElementTagProcessor(dialectPrefix));
 		processadores.add(new MessageBackofficeElementTagProcessor(dialectPrefix));
 		processadores.add(new MessageSiteElementTagProcessor(dialectPrefix));
 		return processadores;
