364d435cf8b53bb618d76ff843e519f4cbd015d4
Elton Rodrigues <eltonrms.leite@hotmail.com>
2017-11-06 12:42:34 -0200

    Refeito o hashcode da item pedido.

--- src/main/java/br/senac/tads4/piiv/model/ItemPedido.java
+++ src/main/java/br/senac/tads4/piiv/model/ItemPedido.java
@@ -85,6 +85,10 @@ public class ItemPedido implements Serializable {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((pedido == null) ? 0 : pedido.hashCode());
+		result = prime * result + ((produto == null) ? 0 : produto.hashCode());
+		result = prime * result + ((quantidade == null) ? 0 : quantidade.hashCode());
+		result = prime * result + ((valorUnitario == null) ? 0 : valorUnitario.hashCode());
 		return result;
 	}
 
@@ -101,6 +105,26 @@ public class ItemPedido implements Serializable {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
+			return false;
+		if (pedido == null) {
+			if (other.pedido != null)
+				return false;
+		} else if (!pedido.equals(other.pedido))
+			return false;
+		if (produto == null) {
+			if (other.produto != null)
+				return false;
+		} else if (!produto.equals(other.produto))
+			return false;
+		if (quantidade == null) {
+			if (other.quantidade != null)
+				return false;
+		} else if (!quantidade.equals(other.quantidade))
+			return false;
+		if (valorUnitario == null) {
+			if (other.valorUnitario != null)
+				return false;
+		} else if (!valorUnitario.equals(other.valorUnitario))
 			return false;
 		return true;
 	}
