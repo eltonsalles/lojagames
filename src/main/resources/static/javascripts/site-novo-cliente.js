7834f22971b441deafd7d1e70d2a3b8313547197
Douglas Oliveira <douglas.oliveira4@outlook.com>
2017-11-07 22:09:01 -0200

    Crud-Cliente

--- src/main/resources/static/javascripts/site-novo-cliente.js
+++ src/main/resources/static/javascripts/site-novo-cliente.js
@@ -1,8 +1,26 @@
-  $('.datepicker').pickadate({
-    selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15, // Creates a dropdown of 15 years to control year,
-    today: 'Today',
-    clear: 'Clear',
-    close: 'Ok',
-    closeOnSelect: false // Close upon selecting a date,
+var diaSemana = [ 'Domingo', 'Segunda-Feira', 'Terca-Feira', 'Quarta-Feira', 'Quinta-Feira', 'Sexta-Feira', 'Sabado' ];
+  var mesAno = [ 'Janeiro', 'Fevereiro', 'Marco', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro'           , 'Dezembro' ];
+  var data = new Date();
+  var hoje = diaSemana[data.getDay()] + ', ' + mesAno[data.getMonth()] + ' de ' + data.getFullYear();
+  $("#dataNascimento").attr("value", hoje);
+  $(".datepicker").pickadate({
+    monthsFull: mesAno,
+    monthsShort: [ 'Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez' ],
+    weekdaysFull: diaSemana,
+    weekdaysShort: [ 'Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sab' ],
+    weekdaysLetter: [ 'D', 'S', 'T', 'Q', 'Q', 'S', 'S' ],
+    selectMonths: true,
+    selectYears: true,
+    clear: true,
+    format: 'yyyy/mm/dd',
+    today: "Hoje",
+    close: "Fechar",
+    min: new Date(data.getFullYear() - 1, 0, 1),
+    max: new Date(data.getFullYear() + 1, 11, 31),
+    closeOnSelect: true
+  });
+
+  $("#dataNascimento").click(function (event) {
+      event.stopPropagation();
+      $(".datepicker").first().pickadate("picker").open();
   });
