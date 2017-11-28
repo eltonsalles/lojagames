8e6fc300cb7fda74681128278e2356cb641444b4
FabianoBDCarvalho <ffcarvalho426@gmail.com>
2017-11-28 21:02:27 -0200

    If na previsão de entra no html MeusPedidos & alteração da variavel total para BigDecimal no controller relatorio

--- src/main/java/br/senac/tads4/piiv/controller/RelatorioVendaController.java
+++ src/main/java/br/senac/tads4/piiv/controller/RelatorioVendaController.java
@@ -1,4 +1,5 @@
 package br.senac.tads4.piiv.controller;
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,18 +28,25 @@ public class RelatorioVendaController {
 		@RequestMapping(value = "/vendas")
 		public ModelAndView pedido(PedidoFilter pedidoFilter) {
 			ModelAndView mv = new ModelAndView("backoffice/relatorio/RelatorioVendas");
-			double total = 0;
-			
+			BigDecimal total = BigDecimal.ZERO;
+
 			List<Pedido> pedidos =   this.pedidoRepository.filtrar(pedidoFilter);
-			
-			for(int i=0; i<pedidos.size(); i++)
-				total = total + pedidos.get(i).getValorSubTotal().doubleValue();
+			total = this.CalcularTotal(pedidos);
 			
 			mv.addObject("statusAll", StatusPedido.values());
 			mv.addObject("total", total);
 			mv.addObject("listaPedidos",pedidos);
 			
 			return mv;
+		}
+		
+		private BigDecimal CalcularTotal(List<Pedido> pedidos) {
+			BigDecimal total = BigDecimal.ZERO;
+			
+			for (Pedido p : pedidos)
+				total = total.add(p.getValorSubTotal());
+			
+			return total;
 		}
 	
 	
